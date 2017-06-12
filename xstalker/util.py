2814fa5a9cce32f8a2c4088d0373f6cd64e30669
François Gindraud <francois.gindraud@univ-lyon1.fr>
2017-06-12 14:47:57 +0200

    Basic framework adapted from Slam.
    Can connect to xcb, and listen to events as a daemon.
    For now it seems to only catch part of the focus change events.

---
+++ xstalker/util.py
@@ -0,0 +1,110 @@
+# Copyright (c) 2013-2017 Francois GINDRAUD
+# 
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+# 
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+"""
+Utilities
+"""
+
+import select
+import logging, logging.handlers
+
+# Logging
+
+def setup_root_logging (filename, level):
+    root = logging.getLogger ()
+    root.setLevel (level)
+    formatter = logging.Formatter (style = "{", fmt = "{asctime} :: {levelname} :: {name} :: {message}")
+    
+    if filename:
+        output = logging.handlers.RotatingFileHandler (filename, "a", 1000000, 1)
+    else:
+        output = logging.StreamHandler ()
+    
+    output.setLevel (level)
+    output.setFormatter (formatter)
+    root.addHandler (output)
+
+    return root
+
+def setup_logger (module_name):
+    return logging.getLogger (module_name)
+
+logger = setup_logger (__name__)
+
+# Daemon
+
+class Daemon (object):
+    """
+    Daemon objects that listen to file descriptors and can be activated when new data is available
+    A daemon can ask to be reactivated immediately even if no new data is available.
+    A counter ensure that reactivations does not loop undefinitely.
+
+    Must be implemented for each subclass :
+        int fileno () : returns file descriptor
+        bool activate () : do stuff, and returns False to stop the event loop
+    """
+    def activate_manually (self):
+        """ Ask the event loop to activate us again """
+        self._flag_to_be_activated = True
+
+    def _to_be_activated (self):
+        # Try-except handles the init case, where the flag doesn't exist
+        try:
+            return self._flag_to_be_activated
+        except AttributeError:
+            return False
+
+    def _reset_activation_counter (self):
+        self._activation_counter = 0
+    
+    def _activate (self):
+        # If activation counter doesn't exist, we are not in an event loop and we don't care
+        try:
+            self._activation_counter += 1
+            if self._activation_counter > 10:
+                raise RuntimeError ("daemon reactivation loop detected")
+        except AttributeError:
+            pass
+        return self.activate ()
+
+    @staticmethod
+    def event_loop (*daemons):
+        # Quit nicely on SIGTERM
+        import signal
+        def sigterm_handler (sig, stack):
+            import sys
+            sys.exit ()
+        signal.signal (signal.SIGTERM, sigterm_handler)
+        # Event loop itself
+        while True:
+            # Activate deamons until no one has the activation flag raised
+            map (Daemon._reset_activation_counter, daemons)
+            while any (map (Daemon._to_be_activated, daemons)):
+                d = next (filter (Daemon._to_be_activated, daemons))
+                d._flag_to_be_activated = False
+                if d._activate () == False:
+                    return
+
+            # Raise activation flag on all deamons with new input data
+            new_data, _, _ = select.select (daemons, [], [])
+            for d in new_data:
+                d._flag_to_be_activated = True
+
---
+++ xstalker/xcb_backend.py
@@ -0,0 +1,96 @@
+# Copyright (c) 2013-2015 Francois GINDRAUD
+# 
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+# 
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+"""
+XCB interface part of the daemon.
+"""
+
+import xcffib, xcffib.xproto
+
+from . import util
+logger = util.setup_logger (__name__)
+
+class Backend (util.Daemon):
+    ##################
+    # Main Interface #
+    ##################
+
+    def __init__ (self, **kwd):
+        """
+        Backend init. Optionnal arguments :
+            screen, display :
+                override X11 default connect information
+        """
+        self.update_callback = (lambda _: 0)
+        self.init_connection (**kwd)
+
+    def cleanup (self):
+        self.conn.disconnect ()
+    
+    def fileno (self):
+        return self.conn.get_file_descriptor ()
+
+    def activate (self):
+        """ Daemon callback """
+        self.handle_events ()
+        return True # Tell event loop to continue
+
+    def dump (self):
+        """ Returns internal state debug info as a string """
+        return "<TODO>"
+    
+    ############################
+    # Layout Manager Interface # TODO resue for stat module
+    ############################
+
+    def attach (self, callback):
+        """ Register the callback from the manager """
+        self.update_callback = callback
+
+    #################
+    # X11 internals #
+    #################
+
+    def init_connection (self, **kwd):
+        """ Starts connection, construct an initial state, setup events. """
+        # Connection
+        self.conn = xcffib.connect (display = kwd.get ("display"))
+
+        # Internal state 
+        screen_setup = self.conn.setup.roots[kwd.get ("screen", self.conn.pref_screen)]
+        self.root = screen_setup.root
+
+        # Randr register for events
+        mask = xcffib.xproto.EventMask.FocusChange
+        self.conn.core.ChangeWindowAttributes (self.root, xcffib.xproto.CW.EventMask, [mask], is_checked=True)
+        self.conn.flush ()
+
+    def handle_events (self):
+        """ Discards all events, returns True if one was from Randr """
+        ev = self.conn.poll_for_event ()
+        while ev:
+            # Detect if we received at least one randr event
+            if isinstance (ev, xcffib.xproto.FocusInEvent):
+                logger.debug ("[notify] FocusInEvent")
+            elif isinstance (ev, xcffib.xproto.FocusOutEvent):
+                logger.debug ("[notify] FocusOutEvent")
+            ev = self.conn.poll_for_event ()
+
