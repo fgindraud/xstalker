0186ac53ed0017f387b8dad3dbe28eacfaeeb989
François Gindraud <francois.gindraud@univ-lyon1.fr>
2018-06-13 15:29:29 +0200

    Scan db and determine offset of last line

--- src/main.rs
+++ src/main.rs
@@ -105,15 +105,6 @@ impl CategoryDurationCounter {
     }
 }
 
-/// Database
-
-struct Database {
-    file: File,
-    last_line_start_offset: usize,
-    db_categories: Vec<String>,
-    //last_line_time: Option< TIME_STUFF >,
-}
-
 fn is_subchain_of<P, S>(pattern: P, searched: S) -> bool
 where
     P: IntoIterator,
@@ -135,26 +126,40 @@ where
     true
 }
 
+/// Database
+struct Database {
+    file: File,
+    last_line_start_offset: usize,
+    db_categories: Vec<String>,
+    //last_line_time: Option< TIME_STUFF >,
+}
+
 impl Database {
     /// Open a database
     pub fn open(path: &Path, classifier_categories: &Vec<&str>) -> io::Result<Self> {
         match fs::OpenOptions::new().read(true).write(true).open(path) {
             Ok(f) => {
                 let mut reader = io::BufReader::new(f);
-                let db_categories = Database::parse_categories(&mut reader)?;
-                if !is_subchain_of(classifier_categories, &db_categories) {
-                    return Err(io::Error::new(
+                let (db_categories, header_len) = Database::parse_categories(&mut reader)?;
+                if is_subchain_of(classifier_categories, &db_categories) {
+                    let last_line_start_offset =
+                        Database::scan_db_entries(&mut reader, header_len, db_categories.len())?;
+                    // TODO seek to last line, and read it to get time
+                    Ok(Database {
+                        file: reader.into_inner(),
+                        last_line_start_offset: last_line_start_offset,
+                        db_categories: db_categories,
+                    })
+                } else {
+                    // TODO add categories, possibly reorganizing
+                    Err(io::Error::new(
                         io::ErrorKind::Other,
                         format!(
                             "category mismatch: expected {:?}, got {:?}",
                             classifier_categories, &db_categories
                         ),
-                    ));
+                    ))
                 }
-                // TODO Seek to start of last line
-                // TODO make an abstraction for reading / writing while maintaining cached info of
-                // cursor position.
-                unimplemented!()
             }
             Err(ref e) if e.kind() == io::ErrorKind::NotFound => {
                 Database::create_new(path, classifier_categories)
@@ -185,33 +190,70 @@ impl Database {
         })
     }
 
-    fn parse_categories(reader: &mut io::BufReader<File>) -> io::Result<Vec<String>> {
+    /// Parse header line, return categories and line size.
+    fn parse_categories(reader: &mut io::BufReader<File>) -> io::Result<(Vec<String>, usize)> {
         use io::{Error, ErrorKind};
         let mut first_line = String::new();
-        reader.read_line(&mut first_line)?;
+        let header_len = reader.read_line(&mut first_line)?;
         // Line must exist, must be '\n'-terminated, must contain at least 'time' header.
-        if first_line.is_empty() {
+        if header_len == 0 {
             return Err(Error::new(
                 ErrorKind::InvalidData,
                 "database has no header line",
             ));
         }
-        if !first_line.ends_with('\n') {
+        if first_line.pop() != Some('\n') {
             return Err(Error::new(
                 ErrorKind::InvalidData,
                 "database header line is not newline terminated",
             ));
         }
-        let (line, _) = first_line.split_at(first_line.len() - 2);
-        let mut elements = line.split('\t');
+        let mut elements = first_line.split('\t');
         if let Some(_time_header) = elements.next() {
-            Ok(elements.map(|s| String::from(s)).collect())
+            Ok((elements.map(|s| String::from(s)).collect(), header_len))
         } else {
             Err(Error::new(
                 ErrorKind::InvalidData,
                 "database header has no field",
             ))
         }
+    }
+
+    /// Check db entries, return last_line_start_offset
+    /// Assume reader cursor is at start of second line.
+    fn scan_db_entries(
+        reader: &mut io::BufReader<File>,
+        header_len: usize,
+        nb_categories: usize,
+    ) -> io::Result<usize> {
+        use io::{Error, ErrorKind};
+        let mut line = String::new();
+        let mut line_nb = 2; // Start at line 2
+        let mut offset = header_len;
+        let mut prev_line_len = 0;
+        loop {
+            let line_len = reader.read_line(&mut line)?;
+            // Entry line must be either empty, or be '\n'-terminated and have the right fields
+            if line_len == 0 {
+                return Ok(offset);
+            }
+            if line.pop() != Some('\n') {
+                return Err(Error::new(
+                    ErrorKind::InvalidData,
+                    format!("database entry at line {}: not newline terminated", line_nb),
+                ));
+            }
+            if line.split('\t').count() != nb_categories + 1 {
+                return Err(Error::new(
+                    ErrorKind::InvalidData,
+                    format!("database entry at line {}: field count mismatch", line_nb),
+                ));
+            }
+            line_nb += 1;
+            offset += prev_line_len;
+            prev_line_len = line_len;
+        }
+        Ok(offset)
     }
 
     pub fn write_to_disk(&mut self) {
